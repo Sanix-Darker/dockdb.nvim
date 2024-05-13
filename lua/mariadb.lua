507f5da56d2fd268ef407a6c612ac331d0ec713d
sanix-darker <s4nixd@gmail.com>
2024-05-13 13:55:39 +0200

    feat: add mariadb

---
+++ lua/mariadb.lua
@@ -0,0 +1,19 @@
+---@class MariaDB
+local M = {}
+
+-- Build a specific query on MariaDB with a given config
+function M.BuildMariaDBQuery(_, _)
+end
+
+-- Execute a specific query on MariaDB with a given config
+function M.ExecuteMariaDBQuery(_, _)
+    print("MariaDB")
+
+    -- local image_name = 'xxx'
+    -- local cli = 'yyy'
+    -- require('docker').....
+
+    error("NotImplemented")
+end
+
+return M
