f2aa30690282d0079d2f7bb9dc9aa13abd13810c
sanix-darker <s4nixd@gmail.com>
2024-05-13 13:55:51 +0200

    feat: add mssql

---
+++ lua/mssql.lua
@@ -0,0 +1,19 @@
+---@class MsSql
+local M = {}
+
+-- Build a specific query on MsSql with a given config
+function M.BuildMSSqlQuery(_, _)
+end
+
+-- Execute a specific query on MsSql with a given config
+function M.ExecuteMSSqlQuery(_, _)
+    print("MsSql")
+
+    -- local image_name = 'xxx'
+    -- local cli = 'yyy'
+    -- require('docker').....
+
+    error("NotImplemented")
+end
+
+return M
