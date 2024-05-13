5c477144019fb0a845643cea9085f5fb92606e28
sanix-darker <s4nixd@gmail.com>
2024-05-13 13:56:05 +0200

    feat: add oracle

---
+++ lua/oracle.lua
@@ -0,0 +1,19 @@
+---@class Oracle
+local M = {}
+
+-- Build a specific query on Oracle with a given config
+function M.BuildOracleDBQuery(_, _)
+end
+
+-- Execute a specific query on Oracle with a given config
+function M.ExecuteOracleDBQuery(_, _)
+    print("Oracle")
+
+    -- local image_name = 'xxx'
+    -- local cli = 'yyy'
+    -- require('docker').....
+
+    error("NotImplemented")
+end
+
+return M
